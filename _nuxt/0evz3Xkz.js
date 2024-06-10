bb2468120a52cd9f0197c306b2049e98e4342fb0
nougatbyte <nougatbyte@itandstuff.de>
2024-06-11 01:59:33 +0200

    update credentials

--- _nuxt/0evz3Xkz.js
+++
@@ -1,1 +0,0 @@
-import{f as e,o as n,c as o}from"./FFCUu7_l.js";const s=e({__name:"index",setup(t){return(a,c)=>(n(),o("div"))}});export{s as default};
--- _nuxt/DuXKaYlA.js
+++
@@ -1,1 +0,0 @@
-import{f as e,o as t,g as a,w as o,d as r}from"./FFCUu7_l.js";import{V as n}from"./CWPy1xb4.js";const f=e({__name:"menu2-1",setup(s){return(p,c)=>(t(),a(n,{fluid:""},{default:o(()=>[r(" empty page ")]),_:1}))}});export{f as default};
