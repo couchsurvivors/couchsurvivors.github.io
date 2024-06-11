7935137ca775662602531c1dce9eb9a73cdafc4a
nougatbyte <nougatbyte@itandstuff.de>
2024-06-11 23:14:54 +0200

    update

---
+++ _nuxt/C7i1pnLc.js
@@ -0,0 +1,1 @@
+import{g as c}from"./C5E2g_cc.js";import{f as i,E as m,G as g,o as u,g as l,v as p,X as f}from"./EJ2k2Ohz.js";const U=i({__name:"supabase-image",props:{imagePath:String},setup(r){const a=m(""),t=r;async function n(e){return await c("images",e)}async function s(){a.value=await n(t.imagePath)}return g(()=>{s()}),(e,o)=>(u(),l(f,{src:p(a)},null,8,["src"]))}});export{U as _};
---
+++ _nuxt/CkLiHnOZ.js
@@ -0,0 +1,1 @@
+import{f as e,o as t,g as n,bv as _}from"./EJ2k2Ohz.js";const r=e({__name:"tools",setup(a){return(c,s)=>{const o=_;return t(),n(o)}}});export{r as default};
