314dfb9dcf470a8139c2094767d052174ef9dc0b
nougatbyte <nougatbyte@itandstuff.de>
2024-06-18 02:39:47 +0200

    update

--- _nuxt/CmMOJjyZ.js
+++
@@ -1,1 +0,0 @@
-import{a6 as n,ar as o,aw as r,ab as l,aA as i,af as c,A as u}from"./DqY_47VH.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),p=l()({name:"VContainer",props:m(),setup(a,e){let{slots:s}=e;const{rtlClasses:t}=i();return c(()=>u(a.tag,{class:["v-container",{"v-container--fluid":a.fluid},t.value,a.class],style:a.style},s)),{}}});export{p as V};
