5ba9073d0e4f826509c3cff6adc2fa44994aa0b3
nougatbyte <admin@itandstuff.de>
2024-06-20 23:37:13 +0000

    Update generated static files

---
+++ _nuxt/DYypJbJM.js
@@ -0,0 +1,1 @@
+import{d as e,v as t,F as n,bN as _}from"./BFrEVmP9.js";const r=e({__name:"tools",setup(a){return(c,s)=>{const o=_;return t(),n(o)}}});export{r as default};
---
+++ _nuxt/DiI_0scF.js
@@ -0,0 +1,1 @@
+import{a2 as n,an as o,as as r,a7 as l,aw as i,ab as c,A as u}from"./BFrEVmP9.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),C=l()({name:"VContainer",props:m(),setup(a,e){let{slots:s}=e;const{rtlClasses:t}=i();return c(()=>u(a.tag,{class:["v-container",{"v-container--fluid":a.fluid},t.value,a.class],style:a.style},s)),{}}});export{C as V};
