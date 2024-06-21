408f920c7ac006dc70be1baa09048e545075dcf1
nougatbyte <admin@itandstuff.de>
2024-06-21 02:54:55 +0000

    Update generated static files

---
+++ _nuxt/Dv4vvGV4.js
@@ -0,0 +1,1 @@
+import{a3 as n,an as o,as as r,a8 as l,aw as c,ac as i,A as u}from"./BdlqWWT2.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),C=l()({name:"VContainer",props:m(),setup(a,e){let{slots:s}=e;const{rtlClasses:t}=c();return i(()=>u(a.tag,{class:["v-container",{"v-container--fluid":a.fluid},t.value,a.class],style:a.style},s)),{}}});export{C as V};
