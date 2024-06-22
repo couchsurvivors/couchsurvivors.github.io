b8e7acb8c968004c580602affe88248186e21de5
nougatbyte <admin@itandstuff.de>
2024-06-22 18:24:02 +0000

    Update generated static files

---
+++ _nuxt/Ca-lQtZh.js
@@ -0,0 +1,1 @@
+import{d as e,v as t,a8 as n,bE as _}from"./BUKZ6-gO.js";const r=e({__name:"tools",setup(a){return(c,s)=>{const o=_;return t(),n(o)}}});export{r as default};
---
+++ _nuxt/JyNuwsmD.js
@@ -0,0 +1,1 @@
+function a(e,o=!1){return o?`${(e/1e3).toFixed(2)}kg`:`${Math.round(e)}g`}function r(e){const o={};for(const t of e)o[t.category.name]||(o[t.category.name]={name:t.category.name,weight:t.weight*t.amount,color:t.category.color,icon:t.category.icon}),o[t.category.name].weight+=t.weight*t.amount;return o}export{a as f,r as g};
