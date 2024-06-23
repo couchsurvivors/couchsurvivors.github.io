379ee5bfb2bd2e4a89893b300d456f737855b2cb
nougatbyte <admin@itandstuff.de>
2024-06-23 00:35:00 +0000

    Update generated static files

---
+++ _nuxt/_HaEimW3.js
@@ -0,0 +1,1 @@
+import{F as n,G as o,X as r,I as l,aT as i,L as c,A as u}from"./5MB17W0k.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),C=l()({name:"VContainer",props:m(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=i();return c(()=>u(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{C as V};
